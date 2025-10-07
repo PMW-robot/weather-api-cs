01ef6199527eaf88e54ff3b22ed26b0b8095cb64
Quan Do <Q.Do@eml.com.au>
2025-10-07 09:59:13 +0700

    update

--- weather-api/Controllers/WeatherForecastController.cs
+++ weather-api/Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ namespace weather_api.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index + 1)),
                 TemperatureC = Random.Shared.Next(-20, 100),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
